bd2ff23c64ea509fee01d2e966d62459e87a57f8
mairess <trabalho.maires@gmail.com>
2024-02-20 19:55:50 -0300

    fix: price type

--- tests/mocks/products.mock.ts
+++ tests/mocks/products.mock.ts
@@ -3,26 +3,48 @@ import { Product } from '../../src/types/Product';
 const validProduct: Product = {
     'id': 2,
     'name': 'Peixeira do Agreste',
-    'price': 50,
+    'price': "10 peças de ouro",
     'userId': 1,
 }
 
 const createdProduct = {
     'id': 2,
     'name': 'Peixeira do Agreste',
-    'price': 50,
+    'price': "10 peças de ouro",
     'userId': 1,
 }
 
 const missingNameProduct: Product = {
     'id': 2,
     'name': '',
-    'price': 50,
+    'price': "10 peças de ouro",
     'userId': 1,
 }
+
+const createdProducts: Product[] = [
+    {
+      "id": 1,
+      "name": "Excalibur",
+      "price": "10 peças de ouro",
+      "userId": 1
+    },
+    {
+      "id": 2,
+      "name": "Espada Justiceira",
+      "price": "20 peças de ouro",
+      "userId": 1
+    },
+    {
+      "id": 3,
+      "name": "Lira de Orfeu",
+      "price": "1 peça de ouro",
+      "userId": 2
+    },
+  ]
 
 export default {
     validProduct,
     createdProduct,
     missingNameProduct,
+    createdProducts,
   };
