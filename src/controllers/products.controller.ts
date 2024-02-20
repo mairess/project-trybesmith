a1fb54b4fbe5a2c1b154a3097b8a746a1d3c6969
mairess <trabalho.maires@gmail.com>
2024-02-20 18:30:15 -0300

    feat: product controllers funtion list

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -4,14 +4,21 @@ import mapStatusHTTP from '../utils/mapStatusHTTP';
 
 async function create(req: Request, res: Response) {
   const serviceResponse = await productsService.create(req.body);
-
   if (serviceResponse.status !== 'SUCCESSFUL') {
     return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);  
   }
-
   res.status(201).json(serviceResponse.data);
+}
+
+async function list(_req: Request, res: Response) {
+  const serviceResponse = await productsService.list();
+  if (serviceResponse.status !== 'SUCCESSFUL') {
+    return res.status(mapStatusHTTP(serviceResponse.status)).json(serviceResponse.data);  
+  }
+  res.status(200).json(serviceResponse.data);
 }
 
 export default {
   create,
+  list,
 };
