68bc6aa8719e54d1ed07e9054ee31087e6564d53
mairess <trabalho.maires@gmail.com>
2024-02-20 18:05:06 -0300

    test: UNIT, product controllers

--- tests/unit/controllers/products.controller.test.ts
+++ tests/unit/controllers/products.controller.test.ts
@@ -2,6 +2,9 @@ import chai, { expect } from 'chai';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
+import productMocks from '../../mocks/products.mock'
+import productsController from '../../../src/controllers/products.controller';
+import productsService from '../../../src/services/products.service'
 
 chai.use(sinonChai);
 
@@ -10,9 +13,37 @@ describe('ProductsController', function () {
   const res = {} as Response;
 
   beforeEach(function () {
-    res.status = sinon.stub().returns(res);
-    res.json = sinon.stub().returns(res);
+    res.status = sinon.stub().returnsThis();
+    res.json = sinon.stub().returnsThis();
     sinon.restore();
+  });
+
+  it('Creates a product', async function () {
+    // Arrange
+    req.body = productMocks.validProduct;
+    sinon.stub(productsService, 'create').resolves({
+      status: 'SUCCESSFUL',
+      data: productMocks.createdProduct,
+    });
+    // Act
+    await productsController.create(req, res);
+    // Assert
+    expect(res.status).to.have.been.calledWith(201);
+    expect(res.json).to.have.been.calledWith(productMocks.createdProduct);
+  });
+
+  it('Throws an error missing property "name".', async function () {
+    // Arrange
+    req.body = productMocks.missingNameProduct;
+    sinon.stub(productsService, 'create').resolves({
+      status: 'INVALID_DATA',
+      data: { message: 'Name is required' },
+    });
+    // Act
+    await productsController.create(req, res);
+    // Assert
+    expect(res.status).to.have.been.calledWith(400);
+    expect(res.json).to.have.been.calledWith({ message: 'Name is required' });
   });
 
 });
