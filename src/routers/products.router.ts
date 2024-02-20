82dc3b9aabb0a463b4409e40599750fb98710365
mairess <trabalho.maires@gmail.com>
2024-02-20 18:31:09 -0300

    feat: GET route /products

--- src/routers/products.router.ts
+++ src/routers/products.router.ts
@@ -4,5 +4,6 @@ import productsController from '../controllers/products.controller';
 const productsRouter = Router();
 
 productsRouter.post('/products', productsController.create);
+productsRouter.get('/products', productsController.list);
 
 export default productsRouter;
