4b734d983c60a53388c877b46e0c426023458b6d
mairess <trabalho.maires@gmail.com>
2024-02-20 14:37:44 -0300

    feat: POST route /products

---
+++ src/routers/products.router.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import productsController from '../controllers/products.controller';
+
+const productsRouter = Router();
+
+productsRouter.post('/products', productsController.create);
+
+export default productsRouter;
