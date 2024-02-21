23766176957d9c8baaa7b9140ed7163af031d69a
mairess <trabalho.maires@gmail.com>
2024-02-21 15:24:14 -0300

    test: user services, function list

--- tests/unit/services/users.service.test.ts
+++ tests/unit/services/users.service.test.ts
@@ -1,7 +1,19 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
+import userService from '../../../src//services/user.service'
+import userMocks from '../../mocks/user.mock'
+import UserModel from '../../../src/database/models/user.model';
 
 describe('UsersService', function () {
   beforeEach(function () { sinon.restore(); });
-
+  it('Returna all users with their productsId.', async function () {
+    // Arrange
+    const mockCreateReturn = UserModel.bulkBuild((userMocks.CreatedUsersWithProducts) as any);
+    sinon.stub(UserModel, 'findAll').resolves(mockCreateReturn);
+    // Act
+    const serviceResponse = await userService.list();
+    // Assert
+    expect(serviceResponse.status).to.eq('SUCCESSFUL');
+    // expect(serviceResponse.data).to.deep.equal(userMocks.CreatedUsersWithProducts);
+  })
 });
