15a2187a8b7c379a03d19c65ddf8d3bc2849b978
mairess <trabalho.maires@gmail.com>
2024-02-21 14:40:55 -0300

    feat: POST route /login

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import productsRouter from './routers/products.router';
 import userRouter from './routers/user.router';
+import loginRouter from './routers/login.router';
 
 const app = express();
 
 app.use(express.json());
+app.use(loginRouter);
 app.use(productsRouter);
 app.use(userRouter);
 
