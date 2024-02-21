e7af4cbd82c2f8b00bd9da250f80afaf3ef16a34
mairess <trabalho.maires@gmail.com>
2024-02-21 20:15:30 -0300

    feat: validate products input

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,13 +1,23 @@
 import ProductModel, { ProductSequelizeModel } from '../database/models/product.model';
+import UserModel from '../database/models/user.model';
 import { Product } from '../types/Product';
 import { ServiceResponse } from '../types/ServiceResponse';
-import validateParams from '../utils/validateProductParams';
+import validateProduct from './validations/validateProduct';
 
-async function create(product:Product): Promise<ServiceResponse<Product>> {
+async function create(product: Product): Promise<ServiceResponse<Product>> {
   let responseService: ServiceResponse<Product>;
-  const error = validateParams(product);
+  const error = validateProduct(product);
+  const noUserIdMessage = '"userId" is required';
+  if (!product.userId) return { status: 'BAD_REQUEST', data: { message: noUserIdMessage } };
   if (error) {
-    responseService = { status: 'BAD_REQUEST', data: { message: error } };
+    responseService = { 
+      status: error.data.message.includes('required') ? 'BAD_REQUEST' : 'UNPROCESSABLE_CONTENT', 
+      data: { message: error.data.message } };
+    return responseService;
+  }
+  const user = await UserModel.findByPk(product.userId);
+  if (!user) {
+    responseService = { status: 'UNPROCESSABLE_CONTENT', data: { message: '"userId" not found' } };
     return responseService;
   }
   const newProduct = await ProductModel.create(product);
@@ -20,7 +30,4 @@ async function list(): Promise<ServiceResponse<ProductSequelizeModel[]>> {
   return { status: 'SUCCESSFUL', data: products };
 }
 
-export default {
-  create,
-  list,
-};
+export default { create, list };
