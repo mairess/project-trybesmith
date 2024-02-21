99532156118e90c61f7511861a2f8120efa9f2e2
mairess <trabalho.maires@gmail.com>
2024-02-21 10:05:16 -0300

    feat: user services, function list

---
+++ src/services/user.service.ts
@@ -0,0 +1,32 @@
+import ProductModel from '../database/models/product.model';
+import UserModel from '../database/models/user.model';
+import { ServiceResponse } from '../types/ServiceResponse';
+import { UserWithProductsIdModel } from '../types/User';
+
+type UserWithProductsId = {
+  username: string,
+  productIds: number[],
+};
+
+async function list(): Promise<ServiceResponse<UserWithProductsId[]>> {
+  const users = await UserModel.findAll({
+    attributes: ['username'],
+    include: [
+      { model: ProductModel,
+        as: 'productIds', 
+        attributes: ['id'],
+      },
+    ],
+  });
+
+  const usersWithProductIds = (users as UserWithProductsIdModel[]).map(({ dataValues: user }) => ({
+    username: user.username,
+    productIds: user.productIds.map((product) => product.id),
+  }));
+
+  return { status: 'SUCCESSFUL', data: usersWithProductIds || [] };
+}
+
+export default {
+  list,
+};
