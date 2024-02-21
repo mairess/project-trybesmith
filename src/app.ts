e00afafcad317d24aba708101fb6a81c61900569
mairess <trabalho.maires@gmail.com>
2024-02-21 10:07:45 -0300

    feat: GET route /users

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import productsRouter from './routers/products.router';
+import userRouter from './routers/user.router';
 
 const app = express();
 
 app.use(express.json());
 app.use(productsRouter);
+app.use(userRouter);
 
 export default app;
