91f15f912aea854a30f8ffbf9fecd523b5ad2ea0
mairess <trabalho.maires@gmail.com>
2024-04-13 09:58:45 -0300

    docs: add swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
 import express, { NextFunction, Request, Response } from 'express';
+import swaggerUi, { JsonObject } from 'swagger-ui-express';
+import yaml from 'js-yaml';
+import fs from 'fs';
 import productsRouter from './routers/products.router';
 import userRouter from './routers/user.router';
 import loginRouter from './routers/login.router';
 import mapStatusHTTP from './utils/mapStatusHTTP';
 
 const genericErrorMessage500 = 'Unexpected error!';
+const raw = fs.readFileSync('swagger.yaml', 'utf-8');
+const swaggerDocument = yaml.load(raw) as JsonObject;
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(loginRouter);
 app.use(productsRouter);
 app.use(userRouter);
